fbc827c7368143326d8cc069b1d8866c37acbf0c
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-11 23:03:05 +0100

    Create install.sh

---
+++ locall.host/install.sh
@@ -0,0 +1,9 @@
+dotnet-install.sh  [--architecture <ARCHITECTURE>] [--azure-feed]
+    [--channel <CHANNEL>] [--dry-run] [--feed-credential]
+    [--install-dir <DIRECTORY>] [--jsonfile <JSONFILE>]
+    [--no-path] [--quality <QUALITY>]
+    [--runtime <RUNTIME>] [--runtime-id <RID>]
+    [--skip-non-versioned-files] [--uncached-feed] [--keep-zip] [--zip-path <PATH>] [--verbose]
+    [--version <VERSION>]
+
+dotnet-install.sh --help
