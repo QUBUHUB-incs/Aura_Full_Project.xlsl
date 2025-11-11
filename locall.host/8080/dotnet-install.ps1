81c4ced43b4f82f6c2eef1972dcab69a64215263
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-11 23:01:33 +0100

    Create dotnet-install.ps1

---
+++ locall.host/8080/dotnet-install.ps1
@@ -0,0 +1,9 @@
+dotnet-install.ps1 [-Architecture <ARCHITECTURE>] [-AzureFeed]
+    [-Channel <CHANNEL>] [-DryRun] [-FeedCredential]
+    [-InstallDir <DIRECTORY>] [-JSonFile <JSONFILE>]
+    [-NoPath] [-ProxyAddress] [-ProxyBypassList <LIST_OF_URLS>]
+    [-ProxyUseDefaultCredentials] [-Quality <QUALITY>] [-Runtime <RUNTIME>]
+    [-SkipNonVersionedFiles] [-UncachedFeed] [-KeepZip] [-ZipPath <PATH>] [-Verbose]
+    [-Version <VERSION>]
+
+Get-Help ./dotnet-install.ps1
